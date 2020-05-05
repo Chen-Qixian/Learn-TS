32fafd21c331d898a565c8eb6b16c9474b9809b2
chenqixian <chenqixian@buaa.edu.cn>
2020-05-05 13:42:36 +0800

    action helper

--- vue-ts/src/main.ts
+++ vue-ts/src/main.ts
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+
 import Category from './model/CateEnum'
 import ItemData from './model/ItemData'
+import ActionHelper from './store/ActionHelper'
 
 Vue.config.productionTip = false
 
@@ -9,5 +11,20 @@ new Vue({
   render: h => h(App),
 }).$mount('#app')
 
-let item1 = new ItemData(1, Category.Study, 'title', 'this is content')
+// 测试创建笔记项
+let item1 = new ItemData(-1, Category.Study, 'title', 'this is content')
 console.log(item1)
+
+let ah = new ActionHelper()
+// 测试新增笔记
+// item1.id = ah.add(item1)
+// console.log(ah.memoList)
+
+// 测试修改笔记
+// let item = new ItemData(2, Category.Life, 'life', 'new life')
+// ah.edit(item)
+// console.log(ah.memoList)
+
+// 测试删除笔记
+// ah.remove(1)
+// console.log(ah.memoList)
---
+++ vue-ts/src/store/ActionHelper.ts
@@ -0,0 +1,69 @@
+import DataHelper from './DataHelper'
+import ItemData from '../model/ItemData'
+
+class ActionHelper {
+  // 数据处理
+  dataHelper: DataHelper = new DataHelper('memoData', 'id')
+  memoList: Array<ItemData>
+
+  constructor () {
+    this.memoList = this.readData()
+  }
+  // 读现有数据
+  readData(): Array<ItemData> {
+    let arrObj = this.dataHelper.readData()
+    // obj转ItemData格式
+    let arrItem = arrObj.map((ele: any) => {
+
+      let item = new ItemData()
+
+      item.id = ele.id
+      item.categoryId = ele.categoryId
+      item.title = ele.title
+      item.content = ele.content
+      item.createTime = ele.createTime
+
+      return item
+    })
+
+    return arrItem
+  }
+  // 新增笔记
+  add(item: ItemData): number {
+
+    item.id = this.dataHelper.addData(item)
+    this.memoList.push(item)
+    this.dataHelper.saveData(this.memoList)
+
+    return item.id
+  }
+  // 修改笔记
+  edit(item: ItemData): void {
+    let editItem: ItemData | undefined = this.memoList.find((ele: any) => {
+      return ele.id === item.id
+    })
+
+    if(editItem) {
+
+      editItem.categoryId = item.id
+      editItem.title = item.title
+      editItem.content = item.content
+
+      this.dataHelper.saveData(this.memoList)
+    }
+  }
+  // 删除笔记
+  remove(id: number): void {
+    let removeIndex: number = this.memoList.findIndex((ele: any) => {
+      return ele.id === id
+    })
+
+    if(removeIndex > -1) {
+      this.memoList.splice(removeIndex, 1)
+      
+      this.dataHelper.saveData(this.memoList)
+    }
+  }
+}
+
+export default ActionHelper
